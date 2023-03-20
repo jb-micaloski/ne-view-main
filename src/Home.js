4d52cc96190cd258a47c5c6e14e3e87d26d958b0
jb-micaloski <65561822+jb-micaloski@users.noreply.github.com>
2023-03-19 23:17:08 -0300

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const backgroundStyle = {
+  backgroundImage:
+    "url(https://tecnodefesa.com.br/wp-content/uploads/2016/08/Imagem-1-N.E.Brasil-35anos.jpg)",
+  height: "auto", // altura da página
+  backgroundSize: "cover", // dimensionamento da imagem
+  display: "flex",
+  justifyContent: "center",
+  backgroundColor: "black",
+  alignItems: "middle"
+};
+
+const textStyle = {
+  alignItems: "center",
+  height: "auto",
+  width: "auto",
+  borderRadius: "10%",
+  backgroundColor: "rgb(142, 141, 148)"
+  
+};
+
+const linkStyle = {
+  textDecoration: "none", // removendo sublinhado do link
+  color: "blue" // definindo cor antes do clique
+};
+
+const activeLinkStyle = {
+  color: "red" // definindo cor durante o clique
+};
+
 export default class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Bem Vindo ao NE Brasil</h1>
-        <Link to="/profile">Faça a sua visita guiada aqui!</Link>
+      <div style={backgroundStyle}>
+        <div style={textStyle}>
+          <h1>BEM VINDO AO NE BRASIL</h1>
+          <h1>
+            <Link to="/profile" style={linkStyle} activeStyle={activeLinkStyle}>INICIAR VISITA GUIADA</Link>
+          </h1>
+        </div>
       </div>
     );
   }
